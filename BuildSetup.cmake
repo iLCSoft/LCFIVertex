1d37e656ca684e245063c27ec38d5fb7e9ab9254
Jan Engels <jan.engels@desy.de>
2007-07-10 09:47:58 +0000

    updated

--- BuildSetup.cmake
+++ BuildSetup.cmake
@@ -1,7 +1,7 @@
 #############################################################################
 # cmake build setup for LCFIVertex
 #
-# For building lcio with cmake type:
+# For building LCFIVertex with cmake type:
 # (1) $ mkdir build
 # (2) $ cd build
 # (3) $ cmake -C ../BuildSetup.cmake ..
@@ -35,7 +35,6 @@ SET( CMAKE_MODULE_PATH "${ILC_HOME}/CMakeModules/v01-00"
 # Project options
 ###############################################
 
-#SET( BUILD_SHARED_LIBS OFF CACHE BOOL "Set to OFF to build static libraries" FORCE )
 #SET( INSTALL_DOC OFF CACHE BOOL "Set to OFF to skip build/install Documentation" FORCE )
 
 # set cmake build type
@@ -45,6 +44,8 @@ SET( CMAKE_MODULE_PATH "${ILC_HOME}/CMakeModules/v01-00"
 ###############################################
 # Advanced options
 ###############################################
+
+#SET( BUILD_SHARED_LIBS OFF CACHE BOOL "Set to OFF to build static libraries" FORCE )
 
 # installation path for LCFIVertex
 #SET( CMAKE_INSTALL_PREFIX "/foo/bar" CACHE STRING "Where to install LCFIVertex" FORCE )
